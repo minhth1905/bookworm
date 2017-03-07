fa158cbee53df5e10d1ecf986cad299728f2dc4d
Quang LN <nhatquang2810@hotmail.com>
2017-03-08 00:36:40 +0700

    unify code structure

--- imports/components/indexBooks/indexBooks.js
+++ imports/components/indexBooks/indexBooks.js
@@ -6,8 +6,8 @@ import { Books } from '../../api/books.js';
 import template from './indexBooks.html';
 
 class IndexBooksCtrl {
-   constructor($scope) {
-     $scope.viewModel(this);
+  constructor($scope) {
+    $scope.viewModel(this);
     this.helpers({
       categories() {
         var data = [];
@@ -17,28 +17,28 @@ class IndexBooksCtrl {
           var element = category;
           data_category.category = element.name;
           var booksList = Books.find({
-              "category_id": element._id
+            "category_id": element._id
           }).map(function (obj) {
-              return {
-                  "name": obj.name,
-                  "id": obj._id
-              };
+            return {
+              "name": obj.name,
+              "id": obj._id
+            };
           })
-          
+
           data_category.data_book = booksList;
           data.push(data_category);
         });
         console.log(data);
         return data;
       }
-    }) 
-   }
+    })
+  }
 }
- 
+
 export default angular.module('indexBooks', [
   angularMeteor
 ])
   .component('indexBooks', {
     templateUrl: template,
-    controller: IndexBooksCtrl
+    controller: ['$scope',IndexBooksCtrl]
   });
