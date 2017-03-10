2e01af3611a419553cc38b0772c1481486672046
Quang LN <nhatquang2810@hotmail.com>
2017-03-10 10:56:46 +0700

    update views

--- client/index.js
+++ client/index.js
@@ -1,7 +1,9 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
-import { Meteor } from 'meteor/meteor'
+import {
+  Meteor
+} from 'meteor/meteor'
 
 import sidebar from '../imports/ui/components/sidebar/sidebar.js';
 import navbar from '../imports/ui/components/navbar/navbar.js';
@@ -12,14 +14,15 @@ import main from './main.js';
 // var module = angular.module('bookworm-app',[angularMeteor,uiRouter, sidebar.name,navbar.name, main.name]);
 
 
-var module = angular.module('bookworm-app',[angularMeteor,uiRouter,sidebar.name,navbar.name,main.name,
+var module = angular.module('bookworm-app', [angularMeteor, uiRouter, sidebar.name, navbar.name, main.name,
   login.name,
-	register.name
-	]);
+  register.name
+]);
 
 module.config(config);
 module.run(run);
-function config($stateProvider,$locationProvider, $urlRouterProvider,$qProvider) {
+
+function config($stateProvider, $locationProvider, $urlRouterProvider, $qProvider) {
   'ngInject';
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
@@ -30,8 +33,8 @@ function config($stateProvider,$locationProvider, $urlRouterProvider,$qProvider)
   });
 }
 
-function run ($rootScope, $state) {
-  $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+function run($rootScope, $state) {
+  $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
     if (error === 'AUTH_REQUIRED') {
       $state.go('login');
     }
@@ -41,30 +44,19 @@ function run ($rootScope, $state) {
     if ($state.is('login') || $state.is('register')) {
       $state.go('home');
     }
-  });  
+  });
 }
 
 function onReady() {
   angular.bootstrap(document, ['bookworm-app']);
-  
-  $(document).ready(function() {
-    $('.button-collapse').sideNav({
-        menuWidth: 200, // Default is 300
-        // Choose the horizontal origin
-        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        draggable: true // Choose whether you can drag to open on touch screens
-      });
-  });   
 
-  $(document).ready(function() {
+  $(function () {
     $('.button-collapse').sideNav({
-        menuWidth: 200, // Default is 300
-        // Choose the horizontal origin
-        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        draggable: true // Choose whether you can drag to open on touch screens
-      });
+      menuWidth: 250, // Default is 300
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    });
   });
-
 }
 
 if (Meteor.isCordova) {
