321f369b7e9db609b1c25d6b20b4b80af03585a1
Quang LN <nhatquang2810@hotmail.com>
2017-03-11 12:33:20 +0700

    update views

--- client/index.js
+++ client/index.js
@@ -55,6 +55,8 @@ function onReady() {
     closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
     draggable: true // Choose whether you can drag to open on touch screens
   });
+
+  
 }
 
 if (Meteor.isCordova) {
